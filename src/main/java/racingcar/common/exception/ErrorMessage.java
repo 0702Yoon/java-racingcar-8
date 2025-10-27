5a30e67a76f3680abe6a62b767398c6a5e686dd8
0702Yoon <gkstkddbs99@naver.com>
2025-10-27 21:23:35 +0900

    feat: 애플리케이션 전반의 에러 메시지를 관리하는 열거형
    [주요 변동 사항]
    - format()을 통한 동적 메시지 생성
    - toString() 재정의를 통한 기본 메시지 반환
    
    [논의 사항]
    toString은 디버깅용, 개발자용이라고 생각이 들어, 추후의 네이밍을 변경해야할 것 같습니다.

---
+++ src/main/java/racingcar/common/exception/ErrorMessage.java
@@ -0,0 +1,26 @@
+package racingcar.common.exception;
+
+
+public enum ErrorMessage {
+
+    CAR_NAME_TOO_LONG("자동차 이름은 %d 이하여야 합니다."),
+    CAR_NAME_EMPTY("자동차 이름은 비어 있을 수 없습니다."),
+    INVALID_ATTEMPT_COUNT("시도 횟수는 양수여야 합니다."),
+    DUPLICATE_CAR_NAME("자동차 이름은 중복될 수 없습니다."),
+    INPUT_CANNOT_BE_BLANK("입력값은 비어있거나 공백만으로 이루어질 수 없습니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String format(Object... args) {
+        return String.format(message, args);
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
