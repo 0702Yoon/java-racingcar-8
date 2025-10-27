efcba1a2f9bf0ec38eb2fdd21cc213364d161c0e
0702Yoon <gkstkddbs99@naver.com>
2025-10-27 21:13:37 +0900

    feat: 거리 값을 표현하는 불변 값 객체 구현

---
+++ src/main/java/racingcar/domain/car/Distance.java
@@ -0,0 +1,14 @@
+package racingcar.domain.car;
+
+public class Distance {
+
+    private final int distance;
+
+    public Distance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+}
