0dc214a707dd90e7bfbc66c380b9f86f0faf6933
0702Yoon <gkstkddbs99@naver.com>
2025-10-31 10:18:39 +0900

    fix(ConsoleOutput): 접근제어자를 private를 변경

--- src/main/java/racingcar/presentation/output/ConsoleOutput.java
+++ src/main/java/racingcar/presentation/output/ConsoleOutput.java
@@ -1,8 +1,8 @@
 package racingcar.presentation.output;
 
 public class ConsoleOutput {
-    static final String ASK_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    static final String ASK_NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String ASK_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ASK_NUMBER_OF_ATTEMPTS_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public void printAskCarNames() {
         System.out.println(ASK_CAR_NAMES_MESSAGE);
